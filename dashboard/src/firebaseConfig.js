c96cca1b451a595abac09d447dd44bbc7cc87f19
Jesper Persson <Jespe9103@gmail.com>
2024-12-19 13:41:47 +0100

    Moved Firebase config to environment variables

--- dashboard/src/firebaseConfig.js
+++ dashboard/src/firebaseConfig.js
@@ -1,15 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue } from "firebase/database";
 
-// Din Firebase-konfiguration
 const firebaseConfig = {
-  apiKey: "AIzaSyB6DC9yZOmZUxQCkrXwv8sxCcr1uYoDiAo",
-  authDomain: "laundryguardian.firebaseapp.com",
-  databaseURL: "https://laundryguardian-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "laundryguardian",
-  storageBucket: "laundryguardian.firebasestorage.app",
-  messagingSenderId: "393744233954",
-  appId: "1:393744233954:web:d4d6d8ec95474fbb1b75bb"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initiera Firebase
